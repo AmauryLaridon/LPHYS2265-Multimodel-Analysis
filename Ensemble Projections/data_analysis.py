7b3a0b0ad48e16353c42def352385ce2357f37f0
Amaury Laridon <amaurylaridon@protonmail.com>
2023-05-06 11:21:31 +0200

    Add Projection Plots

--- Ensemble Projections/data_analysis.py
+++ Ensemble Projections/data_analysis.py
@@ -76,7 +76,6 @@ Tsumin = PR["Tsumin"]
 ###################################################### Data Analysis #######################################################
 ############################################################################################################################
 
-
 ##################################################### Tools function #######################################################
 
 ##### Mean computation #####
@@ -249,6 +248,78 @@ def comp_ENS_MU71():
     print("Standard deviation MU71 = {:.3f}".format(std_MU71))
     print("MSE(ENSEMBLE,MU71) = {:.3f}".format(mse_hi))
     print("r(TSIMAL,MU71) = {:.3f}".format(r))
+    print(
+        "------------------------------------------------------------------------------------"
+    )
+
+
+def comp_TSIMAL_MU71():
+    ##### Comparison between TSIMAL and MU71 #####
+    ### Computation of the error on annual mean ice thickness the TSIMAL mean with respect to MU71 ###
+    mean_ENS_hi = mean_all_mod(data=hi, N=N_days_CTL, N_mod=N_mod_CTL)
+    mean_ENS_month_hi = month_mean(
+        mean_ENS_hi
+    )  # Computation of the month mean of hi ensemble mean #
+    mean_month_TSIMAL_hi = month_mean(
+        hi[:, 13]
+    )  # Computation of the month mean of hi for TSIMAL #
+    mean_TSIMAL_hi, mean_MU71_hi, err_abs, err_rel = err_annual_mean_thick(
+        mean_month_TSIMAL_hi, hi_MU71
+    )
+    mse_hi = MSE_annual_mean_thick(data1=mean_month_TSIMAL_hi, data2=hi_MU71)
+    std_TSIMAL = std_var_mean_thick(data=mean_month_TSIMAL_hi)
+    std_MU71 = std_var_mean_thick(data=hi_MU71)
+    r = cor_annual_mean_thick(data1=mean_month_TSIMAL_hi, data2=hi_MU71)
+    print(
+        "-------------------- TSIMAL & MU71 ICE THICKNESS COMPARISON ----------------------"
+    )
+    print(
+        "------------------------------------------------------------------------------------"
+    )
+    print("Mean ice thickness TSIMAL = {:.3f}m".format(mean_TSIMAL_hi))
+    print("Mean ice thickness MU71 = {:.3f}m".format(mean_MU71_hi))
+    print("Absolute Error = {:.4f}m".format(err_abs))
+    print("Relative Error = {:.3f}%".format(err_rel))
+    print("Standard deviation TSIMAL = {:.3f}".format(std_TSIMAL))
+    print("Standard deviation MU71 = {:.3f}".format(std_MU71))
+    print("MSE(TSIMAL,MU71) = {:.3f}".format(mse_hi))
+    print("r(TSIMAL,MU71) = {:.3f}".format(r))
+    print(
+        "------------------------------------------------------------------------------------"
+    )
+
+
+def comp_SIGUS_MU71():
+    ##### Comparison between SIGUS and MU71 #####
+    ### Computation of the error on annual mean ice thickness the SIGUS mean with respect to MU71 ###
+    mean_ENS_hi = mean_all_mod(data=hi, N=N_days_CTL, N_mod=N_mod_CTL)
+    mean_ENS_month_hi = month_mean(
+        mean_ENS_hi
+    )  # Computation of the month mean of hi ensemble mean #
+    mean_month_SIGUS_hi = month_mean(
+        hi[:, 11]
+    )  # Computation of the month mean of hi for SIGUS #
+    mean_SIGUS_hi, mean_MU71_hi, err_abs, err_rel = err_annual_mean_thick(
+        mean_month_SIGUS_hi, hi_MU71
+    )
+    mse_hi = MSE_annual_mean_thick(data1=mean_month_SIGUS_hi, data2=hi_MU71)
+    std_SIGUS = std_var_mean_thick(data=mean_month_SIGUS_hi)
+    std_MU71 = std_var_mean_thick(data=hi_MU71)
+    r = cor_annual_mean_thick(data1=mean_month_SIGUS_hi, data2=hi_MU71)
+    print(
+        "-------------------- SIGUS & MU71 ICE THICKNESS COMPARISON ----------------------"
+    )
+    print(
+        "------------------------------------------------------------------------------------"
+    )
+    print("Mean ice thickness SIGUS = {:.3f}m".format(mean_SIGUS_hi))
+    print("Mean ice thickness MU71 = {:.3f}m".format(mean_MU71_hi))
+    print("Absolute Error = {:.4f}m".format(err_abs))
+    print("Relative Error = {:.3f}%".format(err_rel))
+    print("Standard deviation SIGUS = {:.3f}".format(std_SIGUS))
+    print("Standard deviation MU71 = {:.3f}".format(std_MU71))
+    print("MSE(SIGUS,MU71) = {:.3f}".format(mse_hi))
+    print("r(SIGUS,MU71) = {:.3f}".format(r))
     print(
         "------------------------------------------------------------------------------------"
     )
@@ -461,62 +532,111 @@ def comp_SIGUS_ENS():
 ##### Display #####
 
 
-def plot_all_mod(data, data_name, N_mod):
+def plot_all_mod(data, data_name, N_mod, extra_label):
+    figure = plt.figure(figsize=(16, 10))
+
+    if data_name == "hi_mean_month":
+        lab_size_fact = 0.5
+        lab_size_fact_mod = 0.75
+    else:
+        lab_size_fact = 1
+        lab_size_fact_mod = 1
     if np.shape(data)[0] == 365:
         time_range = time_range_ctl
+        time_range_mu = time_range_MU71
         Nbre = N_days_CTL
         label_x = "Days"
     if np.shape(data)[0] == 12:
         time_range = time_range_ctl_month
+        time_range_mu = time_range_MU71_month
         Nbre = N_month_CTL
         label_x = "Month"
+    elif np.shape(data)[0] == 100:
+        time_range = time_range_pr
+        Nbre = N_years_PR
+        label_x = "Year"
     for model in range(N_mod):
-        plt.plot(time_range, data[:, model])
+        plt.plot(time_range, data[:, model], linewidth=1 * lab_size_fact_mod)
     if data_name == "hi":
         plt.plot(
-            time_range_ctl_month,
+            time_range_mu,
             hi_MU71,
             label=r"$h_{i_{MU71}}$",
-            linewidth=4,
+            linewidth=4 * lab_size_fact,
+            color="tab:green",
+        )
+    if data_name == "hi_mean_month":
+        plt.plot(
+            time_range_mu,
+            hi_MU71,
+            label=r"$h_{i_{MU71}}$",
+            linewidth=4 * lab_size_fact,
             color="tab:green",
         )
     mod_mean = mean_all_mod(data=data, N=Nbre, N_mod=N_mod)
     # print(mod_mean)
-    plt.plot(time_range, mod_mean, linewidth=4, color="tab:blue", label=r"Models Mean")
-    plt.title(
-        r"Comparison between ensemble members ($N_{mod} = 15$), their averages and the observation series (MU71)",
-        size=24,
+    plt.plot(
+        time_range,
+        mod_mean,
+        linewidth=4 * lab_size_fact,
+        color="tab:blue",
+        label=r"Models Mean",
     )
-    plt.xlabel(label_x, size=20)
-    if data_name == "hi" or data_name == "hi_mean_month":
+    N_mod_str = str(N_mod)
+    plt.title(
+        r"Comparison between ensemble members"
+        + r"($N_{mod}$ ="
+        + N_mod_str
+        + ")"
+        + r" and their averages "
+        + extra_label
+        + " simulation",
+        size=24 * lab_size_fact,
+    )
+    plt.xlabel(label_x, size=25 * lab_size_fact)
+    if (
+        data_name == "hi"
+        or data_name == "hi_mean_month"
+        or data_name == "himax"
+        or data_name == "himean"
+        or data_name == "himin"
+    ):
         unit = "[m]"
-    if data_name == "Tsu" or data_name == "Tsu_mean_month":
+    if data_name == "Tsu" or data_name == "Tsu_mean_month" or data_name == "Tsumin":
         unit = "[°K]"
-    if data_name == "hs" or data_name == "hs_mean_month":
+    if data_name == "hs" or data_name == "hs_mean_month" or data_name == "hsmax":
         unit = "[m]"
-    plt.ylabel(data_name + unit, size=20)
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
+    plt.ylabel(data_name + unit, size=25 * lab_size_fact)
+    plt.xticks(fontsize=20 * lab_size_fact)
+    plt.yticks(fontsize=20 * lab_size_fact)
     plt.grid()
-    plt.legend(fontsize=20)
-    plt.savefig(save_dir + data_name + "_all_mod.png", dpi=300)
+    plt.legend(fontsize=20 * lab_size_fact)
+    plt.savefig(save_dir + data_name + "_all_mod_" + extra_label + ".png", dpi=300)
     # plt.show()
     plt.clf()
 
 
-def subplot_all_mod(data1, data2, data3, data_name, N_mod):
+def subplot_all_mod(data1, data2, data3, data_name, N_mod, extra_label):
+    figure = plt.figure(figsize=(16, 10))
+
     # Create 2x2 sub plots
     gs = gridspec.GridSpec(2, 2, wspace=0.25, hspace=0.3, left=0.1, right=0.98)
     plt.figure()
-    plt.suptitle(r"Ensemble model output $(h_i, T_{su}, h_s)$ for the CTL simulation")
+    plt.suptitle(
+        r"Ensemble model output $(h_i, T_{su}, h_s)$ for the "
+        + extra_label
+        + " simulation"
+    )
 
     if np.shape(data1)[0] == 365:
         time_range = time_range_ctl
+        time_range_mu = time_range_MU71
         Nbre = N_days_CTL
         label_x = "Days"
         x_ticks = np.arange(0, N_days_CTL, 50)
     if np.shape(data1)[0] == 12:
         time_range = time_range_ctl_month
+        time_range_mu = time_range_MU71_month
         Nbre = N_month_CTL
         label_x = "Month"
         x_ticks = np.arange(0, N_month_CTL, 2)
@@ -525,7 +645,7 @@ def subplot_all_mod(data1, data2, data3, data_name, N_mod):
     for model in range(N_mod):
         plt.plot(time_range, data1[:, model], linewidth=0.5)
     plt.plot(
-        time_range_ctl_month,
+        time_range_mu,
         hi_MU71,
         label=r"$h_{i_{MU71}}$",
         linewidth=2,
@@ -570,61 +690,71 @@ def subplot_all_mod(data1, data2, data3, data_name, N_mod):
     plt.grid()
     plt.legend(fontsize=10)
 
-    plt.savefig(save_dir + "subplot_all_mod.png", dpi=300)
+    plt.savefig(save_dir + "subplot_all_mod_" + extra_label + ".png", dpi=300)
     plt.clf()
 
 
-def subplot_TSIMAL_SIGUS_ENS():
+def subplot_TSIMAL_SIGUS_ENS(data1, data2, data3, N_mod, extra_label):
+    figure = plt.figure(figsize=(16, 10))
     gs = gridspec.GridSpec(2, 2, wspace=0.25, hspace=0.3, left=0.1, right=0.98)
     plt.figure()
     plt.suptitle(
-        r"Ensemble model and TISMAL\&SIGUS output $(h_i, T_{su}, h_s)$ for the CTL simulation"
+        r"Ensemble model and TISMAL\&SIGUS "
+        + r"output $(h_i, T_{su}, h_s)$ "
+        + extra_label
+        + " simulation"
     )
+    if np.shape(data1)[0] == 365:
+        time_range = time_range_ctl
+        Nbre = N_days_CTL
+        label_x = "Days"
+    if np.shape(data1)[0] == 12:
+        time_range = time_range_ctl_month
+        Nbre = N_month_CTL
+        label_x = "Month"
+    elif np.shape(data1)[0] == 100:
+        time_range = time_range_pr
+        Nbre = N_years_PR
+        label_x = "Year"
     ### Figure 1 ###
     ax = plt.subplot(gs[0, 0])  # row 0, col 0
-    plt.plot(time_range_ctl, hi[:, 13], linewidth=1, label="TSIMAL", color="red")
-    plt.plot(time_range_ctl, hi[:, 11], linewidth=1, label="SIGUS", color="orange")
-    mod_mean = mean_all_mod(data=hi, N=N_days_CTL, N_mod=N_mod_CTL)
-    plt.plot(
-        time_range_ctl, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean"
-    )
+    plt.plot(time_range, data1[:, 13], linewidth=1, label="TSIMAL", color="red")
+    plt.plot(time_range, data1[:, 11], linewidth=1, label="SIGUS", color="orange")
+    mod_mean = mean_all_mod(data=data1, N=Nbre, N_mod=N_mod)
+    plt.plot(time_range, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean")
     plt.xlabel("Days", size=10)
     plt.ylabel("Ice Thickness [m]", size=10)
-    plt.xticks(np.arange(0, N_days_CTL, 50), fontsize=10)
+    plt.xticks(np.arange(0, Nbre, 50), fontsize=10)
     plt.yticks(fontsize=10)
     plt.grid()
     plt.legend(fontsize=10)
 
     ### Figure 2 ###
     ax = plt.subplot(gs[0, 1])  # row 0, col 1
-    plt.plot(time_range_ctl, Tsu[:, 13], linewidth=1, label="TSIMAL", color="red")
-    plt.plot(time_range_ctl, Tsu[:, 11], linewidth=1, label="SIGUS", color="orange")
-    mod_mean = mean_all_mod(data=Tsu, N=N_days_CTL, N_mod=N_mod_CTL)
-    plt.plot(
-        time_range_ctl, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean"
-    )
+    plt.plot(time_range, data2[:, 13], linewidth=1, label="TSIMAL", color="red")
+    plt.plot(time_range, data2[:, 11], linewidth=1, label="SIGUS", color="orange")
+    mod_mean = mean_all_mod(data=data2, N=Nbre, N_mod=N_mod)
+    plt.plot(time_range, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean")
     plt.xlabel("Days", size=10)
     plt.ylabel("Surface Temperature [°K]", size=10)
-    plt.xticks(np.arange(0, N_days_CTL, 50), fontsize=10)
+    plt.xticks(np.arange(0, Nbre, 50), fontsize=10)
     plt.yticks(fontsize=10)
     plt.grid()
     plt.legend(fontsize=10)
 
     ### Figure 3 ###
     ax = plt.subplot(gs[1, :])  # row 1, span all columns
-    plt.plot(time_range_ctl, hs[:, 13], linewidth=1, label="TSIMAL", color="red")
-    plt.plot(time_range_ctl, hs[:, 11], linewidth=1, label="SIGUS", color="orange")
-    mod_mean = mean_all_mod(data=hs, N=N_days_CTL, N_mod=N_mod_CTL)
-    plt.plot(
-        time_range_ctl, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean"
-    )
+    plt.plot(time_range, data3[:, 13], linewidth=1, label="TSIMAL", color="red")
+    plt.plot(time_range, data3[:, 11], linewidth=1, label="SIGUS", color="orange")
+    mod_mean = mean_all_mod(data=data3, N=Nbre, N_mod=N_mod)
+    plt.plot(time_range, mod_mean, linewidth=2, color="tab:blue", label=r"Models Mean")
     plt.xlabel("Days", size=10)
     plt.ylabel("Snow Thickness [m]", size=10)
-    plt.xticks(np.arange(0, N_days_CTL, 25), fontsize=10)
+    plt.xticks(np.arange(0, Nbre, 25), fontsize=10)
     plt.yticks(fontsize=10)
     plt.grid()
     plt.legend(fontsize=10)
-    plt.savefig(save_dir + "subplot_TSIMAL_SIGUS_ENS.png", dpi=300)
+    plt.savefig(save_dir + "subplot_TSIMAL_SIGUS_ENS_" + extra_label + ".png", dpi=300)
     plt.clf()
 
 
@@ -643,24 +773,34 @@ hi_MU71 = [
     2.72,
     2.75,
 ]
-
+time_range_MU71 = np.linspace(Day_0, N_days_CTL, 12)
+time_range_MU71_month = np.linspace(Day_0, N_month_CTL, 12)
 time_range_ctl = np.arange(
     Day_0, N_days_CTL, 1
 )  # time range for the CTL simulations in days. Used for plot
 time_range_ctl_month = np.arange(
     Month_0, N_month_CTL, 1
 )  # time range for the CTL simulations in month. Used for plot
+time_range_pr = np.arange(
+    Day_0, N_years_PR, 1
+)  # time range for the PR simulations in month. Used for plot
 
 
 if __name__ == "__main__":
     ######################################## Control Simulations Analysis #########################################
     ##### Plot of the ensemble simulations with daily resolution #####
-    plot_all_mod(data=hi, data_name="hi", N_mod=N_mod_CTL)
-    plot_all_mod(data=Tsu, data_name="Tsu", N_mod=N_mod_CTL)
-    plot_all_mod(data=hs, data_name="hs", N_mod=N_mod_CTL)
+    plot_all_mod(data=hi, data_name="hi", N_mod=N_mod_CTL, extra_label="CTL")
+    plot_all_mod(data=Tsu, data_name="Tsu", N_mod=N_mod_CTL, extra_label="CTL")
+    plot_all_mod(data=hs, data_name="hs", N_mod=N_mod_CTL, extra_label="CTL")
     subplot_all_mod(
-        data1=hi, data2=Tsu, data3=hs, data_name=["hi,Tsu,hs"], N_mod=N_mod_CTL
+        data1=hi,
+        data2=Tsu,
+        data3=hs,
+        data_name=["hi,Tsu,hs"],
+        N_mod=N_mod_CTL,
+        extra_label="CTL",
     )
+
     ### Computation of the month mean of the variables ###
     # hi #
     hi_mean_month = np.zeros((12, N_mod_CTL))
@@ -681,17 +821,98 @@ if __name__ == "__main__":
         Tsu_mean_month[:, model] = Tsu_mean_month_mod
     # print(Tsu_mean_month)
     ### Plot of the ensemble simulations with month resolution ###
-    plot_all_mod(data=hi_mean_month, data_name="hi_mean_month", N_mod=N_mod_CTL)
+    plot_all_mod(
+        data=hi_mean_month,
+        data_name="hi_mean_month",
+        N_mod=N_mod_CTL,
+        extra_label="CTL",
+    )
     subplot_all_mod(
         data1=hi_mean_month,
         data2=Tsu,
         data3=hs,
         data_name=["hi_mean_month,Tsu,hs"],
         N_mod=N_mod_CTL,
+        extra_label="CTL",
     )
+
     ########## Verification ###########
     comp_ENS_MU71()
+    comp_TSIMAL_MU71()
+    comp_SIGUS_MU71()
     comp_TSIMAL_ENS()
     comp_SIGUS_ENS()
-    subplot_TSIMAL_SIGUS_ENS()
+    subplot_TSIMAL_SIGUS_ENS(
+        data1=hs, data2=Tsu, data3=hs, N_mod=N_mod_CTL, extra_label="CTL"
+    )
     ######################################## Projection Simulations Analysis #####################################
+    ##### Plot of the ensemble simulations with daily resolution #####
+    ### PR03 ###
+    plot_all_mod(
+        data=himax[:, :, 0], data_name="himax", N_mod=N_mod_PR, extra_label="PR03"
+    )
+    plot_all_mod(
+        data=himean[:, :, 0], data_name="himean", N_mod=N_mod_PR, extra_label="PR03"
+    )
+    plot_all_mod(
+        data=himin[:, :, 0], data_name="himin", N_mod=N_mod_PR, extra_label="PR03"
+    )
+    plot_all_mod(
+        data=hsmax[:, :, 0], data_name="hsmax", N_mod=N_mod_PR, extra_label="PR03"
+    )
+    plot_all_mod(
+        data=Tsumin[:, :, 0], data_name="Tsumin", N_mod=N_mod_PR, extra_label="PR03"
+    )
+    subplot_TSIMAL_SIGUS_ENS(
+        data1=himean[:, :, 0],
+        data2=Tsumin[:, :, 0],
+        data3=hsmax[:, :, 0],
+        N_mod=N_mod_PR,
+        extra_label="PR03",
+    )
+    ### PR06 ###
+    plot_all_mod(
+        data=himax[:, :, 1], data_name="himax", N_mod=N_mod_PR, extra_label="PR06"
+    )
+    plot_all_mod(
+        data=himean[:, :, 1], data_name="himean", N_mod=N_mod_PR, extra_label="PR06"
+    )
+    plot_all_mod(
+        data=himin[:, :, 1], data_name="himin", N_mod=N_mod_PR, extra_label="PR06"
+    )
+    plot_all_mod(
+        data=hsmax[:, :, 1], data_name="hsmax", N_mod=N_mod_PR, extra_label="PR06"
+    )
+    plot_all_mod(
+        data=Tsumin[:, :, 1], data_name="Tsumin", N_mod=N_mod_PR, extra_label="PR06"
+    )
+    subplot_TSIMAL_SIGUS_ENS(
+        data1=himean[:, :, 1],
+        data2=Tsumin[:, :, 1],
+        data3=hsmax[:, :, 1],
+        N_mod=N_mod_PR,
+        extra_label="PR06",
+    )
+    ### PR12 ###
+    plot_all_mod(
+        data=himax[:, :, 2], data_name="himax", N_mod=N_mod_PR, extra_label="PR12"
+    )
+    plot_all_mod(
+        data=himean[:, :, 2], data_name="himean", N_mod=N_mod_PR, extra_label="PR12"
+    )
+    plot_all_mod(
+        data=himin[:, :, 2], data_name="himin", N_mod=N_mod_PR, extra_label="PR12"
+    )
+    plot_all_mod(
+        data=hsmax[:, :, 2], data_name="hsmax", N_mod=N_mod_PR, extra_label="PR12"
+    )
+    plot_all_mod(
+        data=Tsumin[:, :, 2], data_name="Tsumin", N_mod=N_mod_PR, extra_label="PR12"
+    )
+    subplot_TSIMAL_SIGUS_ENS(
+        data1=himean[:, :, 2],
+        data2=Tsumin[:, :, 2],
+        data3=hsmax[:, :, 2],
+        N_mod=N_mod_PR,
+        extra_label="PR12",
+    )
